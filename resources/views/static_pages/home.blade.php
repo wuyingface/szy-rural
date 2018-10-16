42d75a7823616d8a7cea4d40df6c7f0cc92e6e37
wuyingface <xuyingface@163.com>
2018-10-16 07:26:25 +0000

    增加部分样式和局部视图

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Hello Welcome</h1>
+    <p class="lead">
+      村村汇
+    </p>
+    <p>
+      引领深化改革新征程，建设社会主义新农村
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+    </p>
+  </div>
 @stop
