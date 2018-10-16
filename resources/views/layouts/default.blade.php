7ee0bfcf5d7b4d0a1c679f8dc9fcaf2c422d3924
wuyingface <xuyingface@163.com>
2018-10-16 01:59:28 +0000

    添加静态页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <title>@yield('title', '村村汇')- 广东农村信息文化建设</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//静态页面路由
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
